888bf99e37d330b648c577bc0d4c36d723bf1eeb
kareemallen <kareemallen@github.com>
2016-02-05 17:14:44 -0600

    Renamed package to not have dash in name
    TODO: Rename the package to something more descriptive

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'author_email': 'My email.',
     'version': '0.0.1',
     'install_requires': ['nose', 'spotipy'],
-    'packages': ['playlist-exporter'],
+    'packages': ['playlistExporter'],
     'scripts': [],
     'name': ''
 }
